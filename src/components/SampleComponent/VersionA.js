1c76b71e038d8d47de92bba9f137e76af19abdfd
Tomek <tomek@Tomek-MacBook-Pro.local>
2018-07-25 19:21:02 +0200

    build

--- src/components/SampleComponent/VersionA.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const SampleComponentVersionA = () => (
-  <div className="sample-component version-a">This is Version A</div>
-)
-
-export default SampleComponentVersionA;
--- src/components/SampleComponent/VersionB.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const SampleComponentVersionB = () => (
-  <div className="sample-component version-b">This is Version B</div>
-)
-
-export default SampleComponentVersionB;
